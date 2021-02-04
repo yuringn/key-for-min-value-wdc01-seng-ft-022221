a9a765f86529dee74b5e06fbbd9c59ff48c0bf85
Yuri Ng <yuringuyen312@gmail.com>
2021-02-04 01:54:45 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,13 +1,12 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
-def key_for_min_value(name_hash)
-
-  a = nil
-  name_hash.map do |key, value|
-    if value < 15
-  a = key
+def happy_birthday(birthday_kids)
+  birthday_kids.each {|name,age|}
+  puts "Happy birthday #{name}! You are now #{age} years old!"
+  # add your code snippet here!
 end
-end
-p a
-end
-key_for_min_value({:blake => 10, :ashley => 50, :adam => 17})
+happy_birthday({
+  "Timmy" => 9,
+  "Sarah" => 6,
+  "Amanda" => 27
+})
